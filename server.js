ac30af56a953791ce733db7ef79ec7bb3cd08413
luminousbeam <jocelyn.jeriah@gmail.com>
2020-05-26 17:52:47 -0400

    create User model with validations

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const connectDB = require('./config/db')
+const connectDB = require('./config/db');
 
 const app = express();
 
 //Connect DB
 connectDB();
+
+//Init middleware
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) =>
   res.json({ msg: 'Welcome to the ContactKeeper API!' })
