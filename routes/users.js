6447cb70fd76e9923add5c04b6c528274ed89607
luminousbeam <jocelyn.jeriah@gmail.com>
2020-05-26 18:21:29 -0400

    create and respond with Json Web Token

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const { check, validationResult } = require('express-validator');
-
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const User = require('../models/User');
 
 // @route   POST api/users
@@ -42,7 +43,22 @@ router.post(
 
       await user.save();
 
-      res.send('User saved.');
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        {
+          expiresIn: 360000,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server error.');
