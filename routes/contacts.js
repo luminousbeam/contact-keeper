ad2052a35f92cb022b0955116f764069e56ca6e0
luminousbeam <jocelyn.jeriah@gmail.com>
2020-05-27 15:53:32 -0400

    create Contact model and get contacts route

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,32 +1,44 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middleware/auth');
+const { check, validationResult } = require('express-validator');
+const User = require('../models/User');
+const Contact = require('../models/Contact');
 
 // @route   GET api/contacts
 // @desc    Get all users contacts
 // @access  Private
-router.get('/', (req, res) => {
-  res.send('Get all contacts');
+router.get('/', auth, async (req, res) => {
+  try {
+    const contacts = await Contact.find({ user: req.user.id }).sort({
+      date: -1,
+    });
+    res.json(contacts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error.');
+  }
 });
 
 // @route   POST api/contacts
 // @desc    Add new contact
 // @access  Private
 router.post('/', (req, res) => {
-    res.send('Add contact');
-  });
+  res.send('Add contact');
+});
 
 // @route   PUT api/contacts/:id
 // @desc    Add new contact
 // @access  Private
 router.put('/:id', (req, res) => {
-    res.send('Update contact');
-  });
+  res.send('Update contact');
+});
 
 // @route   DELETE api/contacts/:id
 // @desc    Add new contact
 // @access  Private
 router.delete('/:d', (req, res) => {
-    res.send('Delete contact');
-  });
+  res.send('Delete contact');
+});
 
 module.exports = router;
