487d15b6caf494cf31efd02442d1e5d06bc53eed
luminousbeam <jocelyn.jeriah@gmail.com>
2020-05-28 16:31:02 -0400

    initial React setup, cleanuπ, Navbar & Router setup

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About this app</h1>
+      <p className='my-1'>
+        This is a full stack React app for keeping cøntacts.
+      </p>
+      <p className="bg-dark p">
+        Version: 1.0.0
+      </p>
+    </div>
+  );
+};
+
+
+export default About;
