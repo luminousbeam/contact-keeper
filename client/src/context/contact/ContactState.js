6a8a4f51f00fe97852efcfa5de0965d3585ffe05
luminousbeam <jocelyn.jeriah@gmail.com>
2020-05-29 14:28:09 -0400

    add Contacts & ContactItem components, do intial setup for context and state

---
+++ client/src/context/contact/ContactState.js
@@ -0,0 +1,61 @@
+import React, { useReducer } from 'react';
+import uuid from 'uuid';
+import ContactContext from './contactContext';
+import contactReducer from './contactReducer';
+import {
+  ADD_CONTACT,
+  DELETE_CONTACT,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  UPDATE_CONTACT,
+  FILTER_CONTACTS,
+  CLEAR_FILTER,
+} from '../types';
+
+const ContactState = (props) => {
+  const initialState = {
+    contacts: [
+      {
+        id: 1,
+        name: 'Jill Johnson',
+        email: 'jill@email.com',
+        phone: '111-111-1111',
+        type: 'personal',
+      },
+      {
+        id: 2,
+        name: 'Mike Johnson',
+        email: 'mike@email.com',
+        phone: '112-112-1111',
+        type: 'personal',
+      },
+      {
+        id: 3,
+        name: 'Mary Johnson',
+        email: 'mary@email.com',
+        phone: '222-111-1111',
+        type: 'professional',
+      },
+    ],
+  };
+  const [state, dispatch] = useReducer(contactReducer, initialState);
+
+  //Add contact
+  //Delete contact
+  //Set current contact
+  //Clear current contact
+  //Update contact
+  //Filter contacts
+  //Clear filter
+
+  return (
+    <ContactContext.Provider
+      value={{
+        contacts: state.contacts,
+      }}>
+      {props.children}
+    </ContactContext.Provider>
+  );
+};
+
+export default ContactState;
