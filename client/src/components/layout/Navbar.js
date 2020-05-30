88abe10e71dd28cde04024e53d0add267ee28fd5
luminousbeam <jocelyn.jeriah@gmail.com>
2020-05-29 20:56:23 -0400

    Register and Login forms

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -9,12 +9,18 @@ const Navbar = ({ title, icon }) => {
         <i className={icon} /> {title}
       </h1>
       <ul>
-          <li>
-              <Link to="/">Home</Link>
-          </li>
-          <li>
-              <Link to="/about">About</Link>
-          </li>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/about'>About</Link>
+        </li>
+        <li>
+          <Link to='/register'>Register</Link>
+        </li>
+        <li>
+          <Link to='/login'>Login</Link>
+        </li>
       </ul>
     </div>
   );
