c71699b2dd849c6af3088620066f7dbfe2767e25
luminousbeam <jocelyn.jeriah@gmail.com>
2020-06-01 14:24:25 -0400

    added fix to clearContacts issue

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -12,7 +12,7 @@ const Contacts = () => {
   useEffect(() => {
     getContacts();
     // eslint-disable-next-line
-  });
+  },[]);
 
   if (contacts !== null && contacts.length === 0 && !loading) {
     return <h4>Please add a contact</h4>;
